9b1f649f14796ffeee5b1b4a9394ed509ba8f5b0
Jadgptw <Gordin.Alexander90@gmail.com>
2018-09-14 20:07:30 +0300

    index.js implementation has been added.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let result = true;
+    if(str.length > 0){
+        var find = false;
+        for(let i = 0; i < bracketsConfig.length; i++){
+            let brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+            let index = str.indexOf(brackets);
+            if(index >= 0){
+                find = true;
+                str = str.substring(0, index) + str.substring(index + 2);
+                result = check(str, bracketsConfig);
+                break;
+            }
+        }
+        if(!find){
+            result = false;
+        }
+    }
+    return result;
 }
